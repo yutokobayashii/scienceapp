74c1dc907d30ce370ccafc40ae5c113753a22b0e
小林優斗（仕事用） <kobayashiyuutoshigotoyou@kobayashiyuuto.local>
2022-09-17 14:34:44 +0900

    firebase組み込み

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:science/first_page.dart';
 import 'package:science/second_page.dart';
 import 'third_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
--- lib/start_file.dart
+++ lib/start_file.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:science/practicemode.dart';
 import 'package:science/testmode.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 class start_file extends StatelessWidget {
 
@@ -74,6 +77,50 @@ class start_file extends StatelessWidget {
                 },
               ),
             ),
+
+
+    Container(
+          height: double.infinity,
+          alignment: Alignment.topCenter,
+          //1
+          child: StreamBuilder<QuerySnapshot>(
+            //2
+            stream: FirebaseFirestore.instance
+                .collection('dream')
+                .orderBy('createdAt')
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Text('エラーが発生しました');
+              }
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              //3
+              final list = snapshot.requireData.docs
+                  .map<String>((DocumentSnapshot document) {
+                final documentData =
+                document.data()! as Map<String, dynamic>;
+                return documentData['content']! as String;
+              }).toList();
+
+              final reverseList = list.reversed.toList();
+
+              return ListView.builder(
+                itemCount: reverseList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Center(
+                    child: Text(
+                      reverseList[index],
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+    ),
+
           ],
         ),
       ),
